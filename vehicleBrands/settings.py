7e8fc02f22e52d266876aa14acc78e92e33dc631
Colin Murphy <cmurph0327@gmail.com>
2022-02-01 18:33:57 -0500

    Added Brands and Vehicles Table Schemas

--- vehicleBrands/settings.py
+++ vehicleBrands/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'vehicles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'vehicleBrands.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vehiclesdb',
+        'USER': 'postgres',
+        'PASSWORD': '0327',
+        'HOST': '127.0.0.1',
+        'PORT': '5433', 
     }
 }
 
--- vehicles/models.py
+++ vehicles/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Brands(models.Model):
+    name = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return self.name
+
+class Vehicles(models.Model):
+    name = models.CharField(max_length = 50)
+    brand = models.ForeignKey(Brands, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
