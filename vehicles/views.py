e878c8c639d81d268d08e6d412b90e7d8466a956
Colin Murphy <cmurph0327@gmail.com>
2022-02-02 23:55:29 -0500

    Added Brands View and URL

--- vehicles/views.py
+++ vehicles/views.py
@@ -1,3 +1,12 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
+from .models import Brands
+from django.http import HttpResponse
 
 # Create your views here.
+
+def display_brands(request):
+    brands_list = Brands.objects.all()
+    context = {'brands_list': brands_list}
+    return render(request, 'vehicles/display_brands.html', context)
+
