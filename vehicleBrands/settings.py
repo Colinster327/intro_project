4b2d3d86a28e97cbe2995f200fef8b3775826af2
Colin Murphy <cmurph0327@gmail.com>
2022-02-12 13:18:57 -0500

    Added env

--- vehicleBrands/settings.py
+++ vehicleBrands/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -105,9 +109,9 @@ WSGI_APPLICATION = 'vehicleBrands.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'vehiclesdb',
-        'USER': 'postgres',
-        'PASSWORD': '0327',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASS'),
         'HOST': '127.0.0.1',
         'PORT': '5433', 
     }
